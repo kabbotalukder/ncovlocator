7612b86d78d136a7e8841a8f895940708c410324
Kabbo Talukder <63724484+kabbotalukder@users.noreply.github.com>
2020-04-20 00:37:40 +0600

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,534 +1,533 @@
 var nation = 
-    {
-        'update' : '19th April, 8:00PM' ,   
-        'active' : 2456,
-        'death' : 91,
-        'recovered' : 75,  
-    }
+{
+    'update' : '19th April, 11:00PM' ,   
+    'active' : 2456,
+    'death' : 91,
+    'recovered' : 75,  
+}
 
 
 var districts = [
 {
-        'name': 'Dhaka',
-        'cases':{
-            'total_cases': 1014
-        },
-        'coordinates': {
-            'latitude': 23.8103 ,
-            'longitude': 90.4125
-        }
+    'name': 'Dhaka',
+    'cases':{
+        'total_cases': 1014
     },
+    'coordinates': {
+        'latitude': 23.8103 ,
+        'longitude': 90.4125
+    }
+},
 
-    {
-        'name': 'Gazipur',
-        'cases':{
-            'total_cases': 173
-        },
-        'coordinates': {
-            'latitude': 23.9999 ,
-            'longitude': 90.4203
-        }
-    },
 {
-        'name': 'Kishoreganj',
-        'cases':{
-            'total_cases': 77
-        },
-        'coordinates': {
-            'latitude': 24.4260,
-            'longitude': 90.9821
-        }
+    'name': 'Gazipur',
+    'cases':{
+        'total_cases': 173
     },
+    'coordinates': {
+        'latitude': 23.9999 ,
+        'longitude': 90.4203
+    }
+},
 {
-        'name': 'Madaripur',
-        'cases':{
-            'total_cases': 26
-        },
-        'coordinates': {
-            'latitude': 23.2393 ,
-            'longitude': 90.1870
-        }
+    'name': 'Kishoreganj',
+    'cases':{
+        'total_cases': 77
     },
+    'coordinates': {
+        'latitude': 24.4260,
+        'longitude': 90.9821
+    }
+},
 {
-        'name': 'Manikganj',
-        'cases':{
-            'total_cases': 6
-        },
-        'coordinates': {
-            'latitude': 23.8617 ,
-            'longitude': 90.0003
-        }
+    'name': 'Madaripur',
+    'cases':{
+        'total_cases': 26
     },
+    'coordinates': {
+        'latitude': 23.2393 ,
+        'longitude': 90.1870
+    }
+},
 {
-        'name': 'Narayanganj',
-        'cases':{
-            'total_cases': 386
-        },
-        'coordinates': {
-            'latitude': 23.6238 ,
-            'longitude': 90.5000
-        }
+    'name': 'Manikganj',
+    'cases':{
+        'total_cases': 6
     },
+    'coordinates': {
+        'latitude': 23.8617 ,
+        'longitude': 90.0003
+    }
+},
 {
-        'name': 'Munshigonj',
-        'cases':{
-            'total_cases': 33
-        },
-        'coordinates': {
-            'latitude': 23.4981 ,
-            'longitude': 90.4127
-        }
+    'name': 'Narayanganj',
+    'cases':{
+        'total_cases': 386
     },
+    'coordinates': {
+        'latitude': 23.6238 ,
+        'longitude': 90.5000
+    }
+},
 {
-        'name': 'Narsingdi',
-        'cases':{
-            'total_cases': 105
-        },
-        'coordinates': {
-            'latitude': 24.1344 ,
-            'longitude': 90.7860
-        }
+    'name': 'Munshigonj',
+    'cases':{
+        'total_cases': 33
     },
+    'coordinates': {
+        'latitude': 23.4981 ,
+        'longitude': 90.4127
+    }
+},
 {
-        'name': 'Rajbari',
-        'cases':{
-            'total_cases': 7
-        },
-        'coordinates': {
-            'latitude': 23.7639 ,
-            'longitude': 89.6467
-        }
+    'name': 'Narsingdi',
+    'cases':{
+        'total_cases': 105
     },
+    'coordinates': {
+        'latitude': 24.1344 ,
+        'longitude': 90.7860
+    }
+},
 {
-        'name': 'Faridpur',
-        'cases':{
-            'total_cases': 4
-        },
-        'coordinates': {
-            'latitude': 23.6204 ,
-            'longitude': 89.8130
-        }
+    'name': 'Rajbari',
+    'cases':{
+        'total_cases': 7
     },
+    'coordinates': {
+        'latitude': 23.7639 ,
+        'longitude': 89.6467
+    }
+},
 {
-        'name': 'Tangail',
-        'cases':{
-            'total_cases': 10
-        },
-        'coordinates': {
-            'latitude': 24.2513 ,
-            'longitude': 89.9167
-        }
+    'name': 'Faridpur',
+    'cases':{
+        'total_cases': 4
     },
+    'coordinates': {
+        'latitude': 23.6204 ,
+        'longitude': 89.8130
+    }
+},
 {
-        'name': 'Shariatpur',
-        'cases':{
-            'total_cases': 7
-        },
-        'coordinates': {
-            'latitude': 23.2423 ,
-            'longitude': 90.4348
-        }
+    'name': 'Tangail',
+    'cases':{
+        'total_cases': 10
     },
+    'coordinates': {
+        'latitude': 24.2513 ,
+        'longitude': 89.9167
+    }
+},
 {
-        'name': 'Gopalganj',
-        'cases':{
-            'total_cases': 21
-        },
-        'coordinates': {
-            'latitude': 23.0488 ,
-            'longitude': 89.8849
-        }
+    'name': 'Shariatpur',
+    'cases':{
+        'total_cases': 7
     },
- {
-        'name': 'Chattogram',
-        'cases':{
-            'total_cases': 39
-        },
-        'coordinates': {
-            'latitude': 22.3569 ,
-            'longitude': 91.7832
-        }
+    'coordinates': {
+        'latitude': 23.2423 ,
+        'longitude': 90.4348
+    }
+},
+{
+    'name': 'Gopalganj',
+    'cases':{
+        'total_cases': 21
     },
-    {
-        'name': 'Cox’s Bazar',
-        'cases':{
-            'total_cases': 1
-        },
-        'coordinates': {
-            'latitude': 21.4272,
-            'longitude': 92.0058
-        }
+    'coordinates': {
+        'latitude': 23.0488 ,
+        'longitude': 89.8849
+    }
+},
+{
+    'name': 'Chattogram',
+    'cases':{
+        'total_cases': 39
     },
-    {
-        'name': 'Cumilla',
-        'cases':{
-            'total_cases': 19
-        },
-        'coordinates': {
-            'latitude': 23.4607,
-            'longitude': 91.1809
-        }
+    'coordinates': {
+        'latitude': 22.3569 ,
+        'longitude': 91.7832
+    }
+},
+{
+    'name': 'Cox’s Bazar',
+    'cases':{
+        'total_cases': 1
     },
-    {
-        'name': 'B. Baria',
-        'cases':{
-            'total_cases':11
-        },
-        'coordinates': {
-            'latitude': 23.9608,
-            'longitude': 91.1115
-        }
+    'coordinates': {
+        'latitude': 21.4272,
+        'longitude': 92.0058
+    }
+},
+{
+    'name': 'Cumilla',
+    'cases':{
+        'total_cases': 19
     },
-    {
-        'name': 'Lakshmipur',
-        'cases':{
-            'total_cases':21
-        },
-        'coordinates': {
-            'latitude': 22.9447,
-            'longitude': 90.8282
-        }
+    'coordinates': {
+        'latitude': 23.4607,
+        'longitude': 91.1809
+    }
+},
+{
+    'name': 'B. Baria',
+    'cases':{
+        'total_cases':11
     },
-    {
-        'name': 'Bandarban',
-        'cases':{
-            'total_cases': 1
-        },
-        'coordinates': {
-            'latitude': 22.1936,
-            'longitude': 92.2187
-        }
+    'coordinates': {
+        'latitude': 23.9608,
+        'longitude': 91.1115
+    }
+},
+{
+    'name': 'Lakshmipur',
+    'cases':{
+        'total_cases':21
     },
+    'coordinates': {
+        'latitude': 22.9447,
+        'longitude': 90.8282
+    }
+},
+{
+    'name': 'Bandarban',
+    'cases':{
+        'total_cases': 1
+    },
+    'coordinates': {
+        'latitude': 22.1936,
+        'longitude': 92.2187
+    }
+},
 
-    {
-        'name': 'Noakhali',
-        'cases':{
-            'total_cases': 3
-        },
-        'coordinates': {
-            'latitude': 22.8724,
-            'longitude': 91.0921
-        }
-    },
 {
-        'name': 'Chandpur',
-        'cases':{
-            'total_cases':8
-        },
-        'coordinates': {
-            'latitude': 23.2321,
-            'longitude': 90.6631
-        }
- },
- {
-        'name': 'Sylhet',
-        'cases':{
-            'total_cases': 3
-        },
-        'coordinates': {
-            'latitude': 24.8949 ,
-            'longitude': 91.8687
-        }
+    'name': 'Noakhali',
+    'cases':{
+        'total_cases': 3
     },
- {
-        'name': 'Moulvibazar',
-        'cases':{
-            'total_cases': 2
-        },
-        'coordinates': {
-            'latitude': 24.3095 ,
-            'longitude': 91.7315
-        }
-    },
- {
-        'name': 'Sunamganj',
-        'cases':{
-            'total_cases': 1
-        },
-        'coordinates': {
-            'latitude': 25.0715 ,
-            'longitude': 91.3992
-        }
-    },
+    'coordinates': {
+        'latitude': 22.8724,
+        'longitude': 91.0921
+    }
+},
 {
-        'name': 'Habiganj',
-        'cases':{
-            'total_cases': 1
-        },
-        'coordinates': {
-            'latitude': 24.4771 ,
-            'longitude': 91.4507
-        }
+    'name': 'Chandpur',
+    'cases':{
+        'total_cases':8
     },
-    {
-        'name': 'Rangpur',
-        'cases':{
-            'total_cases': 3
-        },
-        'coordinates': {
-            'latitude': 25.7439 ,
-            'longitude': 89.2752
-        }
-    },
+    'coordinates': {
+        'latitude': 23.2321,
+        'longitude': 90.6631
+    }
+},
 {
-        'name': 'Gaibandha',
-        'cases':{
-            'total_cases': 12
-        },
-        'coordinates': {
-            'latitude': 25.3290 ,
-            'longitude': 89.2541
-        }
+    'name': 'Sylhet',
+    'cases':{
+        'total_cases': 3
     },
+    'coordinates': {
+        'latitude': 24.8949 ,
+        'longitude': 91.8687
+    }
+},
 {
-        'name': 'Nilphamari',
-        'cases':{
-            'total_cases': 9
-        },
-        'coordinates': {
-            'latitude': 25.8483 ,
-            'longitude': 88.9414
-        }
+    'name': 'Moulvibazar',
+    'cases':{
+        'total_cases': 2
     },
+    'coordinates': {
+        'latitude': 24.3095 ,
+        'longitude': 91.7315
+    }
+},
 {
-        'name': 'Lalmonirhat',
-        'cases':{
-            'total_cases': 2
-        },
-        'coordinates': {
-            'latitude': 25.9923 ,
-            'longitude': 89.2847
-        }
+    'name': 'Sunamganj',
+    'cases':{
+        'total_cases': 1
     },
+    'coordinates': {
+        'latitude': 25.0715 ,
+        'longitude': 91.3992
+    }
+},
 {
-        'name': 'Kurigram',
-        'cases':{
-            'total_cases': 2
-        },
-        'coordinates': {
-            'latitude': 25.8072 ,
-            'longitude': 89.6252
-        }
+    'name': 'Habiganj',
+    'cases':{
+        'total_cases': 1
     },
-    {
-        'name': 'Dinajpur',
-        'cases':{
-            'total_cases': 10
-        },
-        'coordinates': {
-            'latitude': 25.6249 ,
-            'longitude': 88.6332
-        }
-    },
-    {
-        'name': 'Panchagar',
-        'cases':{
-            'total_cases': 1
-        },
-        'coordinates': {
-            'latitude': 26.2709 ,
-            'longitude': 88.5952
-        }
-    },
+    'coordinates': {
+        'latitude': 24.4771 ,
+        'longitude': 91.4507
+    }
+},
 {
-        'name': 'Thakurgaon',
-        'cases':{
-            'total_cases': 6
-        },
-        'coordinates': {
-            'latitude': 26.0418 ,
-            'longitude': 88.4283
-        }
+    'name': 'Rangpur',
+    'cases':{
+        'total_cases': 3
     },
-    {
-        'name': 'Khulna',
-        'cases':{
-            'total_cases': 1
-        },
-        'coordinates': {
-            'latitude': 22.8456 ,
-            'longitude': 89.5403
-        }
-    },
-    {
-        'name': 'Jessore',
-        'cases':{
-            'total_cases': 1
-        },
-        'coordinates': {
-            'latitude': 23.1778 ,
-            'longitude': 89.1801
-        }
-    },
-    {
-        'name': 'Bagerhat',
-        'cases':{
-            'total_cases': 1
-        },
-        'coordinates': {
-            'latitude': 22.6602 ,
-            'longitude': 89.7895
-        }
-    },
+    'coordinates': {
+        'latitude': 25.7439 ,
+        'longitude': 89.2752
+    }
+},
 {
-        'name': 'Narail',
-        'cases':{
-            'total_cases': 2
-        },
-        'coordinates': {
-            'latitude': 23.1657 ,
-            'longitude': 89.4990
-        }
+    'name': 'Gaibandha',
+    'cases':{
+        'total_cases': 12
     },
+    'coordinates': {
+        'latitude': 25.3290 ,
+        'longitude': 89.2541
+    }
+},
 {
-        'name': 'Chuadanga',
-        'cases':{
-            'total_cases': 1
-        },
-        'coordinates': {
-            'latitude': 23.6161 ,
-            'longitude': 88.8263
-        }
+    'name': 'Nilphamari',
+    'cases':{
+        'total_cases': 9
     },
-    {
-        'name': 'Mymensingh',
-        'cases':{
-            'total_cases': 21
-        },
-        'coordinates': {
-            'latitude': 24.7471 ,
-            'longitude': 90.4203
-        }
-    },
+    'coordinates': {
+        'latitude': 25.8483 ,
+        'longitude': 88.9414
+    }
+},
 {
-        'name': 'Jamalpur',
-        'cases':{
-            'total_cases': 20
-        },
-        'coordinates': {
-            'latitude': 25.0831 ,
-            'longitude': 89.7853
-        }
+    'name': 'Lalmonirhat',
+    'cases':{
+        'total_cases': 2
     },
+    'coordinates': {
+        'latitude': 25.9923 ,
+        'longitude': 89.2847
+    }
+},
 {
-        'name': 'Netrokona',
-        'cases':{
-            'total_cases': 14
-        },
-        'coordinates': {
-            'latitude': 24.8103 ,
-            'longitude': 90.8656
-        }
+    'name': 'Kurigram',
+    'cases':{
+        'total_cases': 2
     },
+    'coordinates': {
+        'latitude': 25.8072 ,
+        'longitude': 89.6252
+    }
+},
 {
-        'name': 'Sherpur',
-        'cases':{
-            'total_cases': 11
-        },
-        'coordinates': {
-            'latitude': 25.0194 ,
-            'longitude': 90.0137
-        }
+    'name': 'Dinajpur',
+    'cases':{
+        'total_cases': 10
     },
-    {
-        'name': 'Barguna',
-        'cases':{
-            'total_cases': 10
-        },
-        'coordinates': {
-            'latitude': 22.0953 ,
-            'longitude': 90.1121
-        }
-    },
-    {
-        'name': 'Barisal',
-        'cases':{
-            'total_cases': 21
-        },
-        'coordinates': {
-            'latitude': 22.7010 ,
-            'longitude': 90.3535
-        }
-    },
+    'coordinates': {
+        'latitude': 25.6249 ,
+        'longitude': 88.6332
+    }
+},
 {
-        'name': 'Barguna',
-        'cases':{
-            'total_cases': 5
-        },
-        'coordinates': {
-            'latitude': 22.0953,
-            'longitude': 90.1121
-        }
+    'name': 'Panchagar',
+    'cases':{
+        'total_cases': 1
     },
+    'coordinates': {
+        'latitude': 26.2709 ,
+        'longitude': 88.5952
+    }
+},
 {
-        'name': 'Patuakhali',
-        'cases':{
-            'total_cases': 2
-        },
-        'coordinates': {
-            'latitude': 22.2249,
-            'longitude': 90.4548
-        }
+    'name': 'Thakurgaon',
+    'cases':{
+        'total_cases': 6
     },
+    'coordinates': {
+        'latitude': 26.0418 ,
+        'longitude': 88.4283
+    }
+},
 {
-        'name': 'Pirojpur',
-        'cases':{
-            'total_cases': 4
-        },
-        'coordinates': {
-            'latitude': 22.5791,
-            'longitude': 89.9759
-        }
+    'name': 'Khulna',
+    'cases':{
+        'total_cases': 1
     },
+    'coordinates': {
+        'latitude': 22.8456 ,
+        'longitude': 89.5403
+    }
+},
 {
-        'name': 'Jhalokathi',
-        'cases':{
-            'total_cases': 4
-        },
-        'coordinates': {
-            'latitude': 22.6406,
-            'longitude': 90.1987
-        }
+    'name': 'Jessore',
+    'cases':{
+        'total_cases': 1
     },
-    {
-        'name': 'Joyputhat',
-        'cases':{
-            'total_cases': 2
-        },
-        'coordinates': {
-            'latitude': 25.0947 ,
-            'longitude': 89.0945
-        }
+    'coordinates': {
+        'latitude': 23.1778 ,
+        'longitude': 89.1801
+    }
+},
+{
+    'name': 'Bagerhat',
+    'cases':{
+        'total_cases': 1
     },
-    {
-        'name': 'Pabna',
-        'cases':{
-            'total_cases': 2
-        },
-        'coordinates': {
-            'latitude': 24.1585 ,
-            'longitude': 89.4481
-        }
+    'coordinates': {
+        'latitude': 22.6602 ,
+        'longitude': 89.7895
+    }
+},
+{
+    'name': 'Narail',
+    'cases':{
+        'total_cases': 2
     },
-    {
-        'name': 'Bogra',
-        'cases':{
-            'total_cases': 1
-        },
-        'coordinates': {
-            'latitude': 24.8481 ,
-            'longitude': 89.3730
-        }
+    'coordinates': {
+        'latitude': 23.1657 ,
+        'longitude': 89.4990
+    }
+},
+{
+    'name': 'Chuadanga',
+    'cases':{
+        'total_cases': 1
     },
-    {
-        'name': 'Rajshahi',
-        'cases':{
-            'total_cases': 4
-        },
-        'coordinates': {
-            'latitude': 24.3745 ,
-            'longitude': 88.6042
-        }
+    'coordinates': {
+        'latitude': 23.6161 ,
+        'longitude': 88.8263
+    }
+},
+{
+    'name': 'Mymensingh',
+    'cases':{
+        'total_cases': 21
     },
+    'coordinates': {
+        'latitude': 24.7471 ,
+        'longitude': 90.4203
+    }
+},
+{
+    'name': 'Jamalpur',
+    'cases':{
+        'total_cases': 20
+    },
+    'coordinates': {
+        'latitude': 25.0831 ,
+        'longitude': 89.7853
+    }
+},
+{
+    'name': 'Netrokona',
+    'cases':{
+        'total_cases': 14
+    },
+    'coordinates': {
+        'latitude': 24.8103 ,
+        'longitude': 90.8656
+    }
+},
+{
+    'name': 'Sherpur',
+    'cases':{
+        'total_cases': 11
+    },
+    'coordinates': {
+        'latitude': 25.0194 ,
+        'longitude': 90.0137
+    }
+},
+{
+    'name': 'Barguna',
+    'cases':{
+        'total_cases': 10
+    },
+    'coordinates': {
+        'latitude': 22.0953 ,
+        'longitude': 90.1121
+    }
+},
+{
+    'name': 'Barisal',
+    'cases':{
+        'total_cases': 21
+    },
+    'coordinates': {
+        'latitude': 22.7010 ,
+        'longitude': 90.3535
+    }
+},
+{
+    'name': 'Barguna',
+    'cases':{
+        'total_cases': 5
+    },
+    'coordinates': {
+        'latitude': 22.0953,
+        'longitude': 90.1121
+    }
+},
+{
+    'name': 'Patuakhali',
+    'cases':{
+        'total_cases': 2
+    },
+    'coordinates': {
+        'latitude': 22.2249,
+        'longitude': 90.4548
+    }
+},
+{
+    'name': 'Pirojpur',
+    'cases':{
+        'total_cases': 4
+    },
+    'coordinates': {
+        'latitude': 22.5791,
+        'longitude': 89.9759
+    }
+},
+{
+    'name': 'Jhalokathi',
+    'cases':{
+        'total_cases': 4
+    },
+    'coordinates': {
+        'latitude': 22.6406,
+        'longitude': 90.1987
+    }
+},
+{
+    'name': 'Joyputhat',
+    'cases':{
+        'total_cases': 2
+    },
+    'coordinates': {
+        'latitude': 25.0947 ,
+        'longitude': 89.0945
+    }
+},
+{
+    'name': 'Pabna',
+    'cases':{
+        'total_cases': 2
+    },
+    'coordinates': {
+        'latitude': 24.1585 ,
+        'longitude': 89.4481
+    }
+},
+{
+    'name': 'Bogura',
+    'cases':{
+        'total_cases': 1
+    },
+    'coordinates': {
+        'latitude': 24.8481 ,
+        'longitude': 89.3730
+    }
+},
+{
+    'name': 'Rajshahi',
+    'cases':{
+        'total_cases': 4
+    },
+    'coordinates': {
+        'latitude': 24.3745 ,
+        'longitude': 88.6042
+    }
+},
 ]
-
