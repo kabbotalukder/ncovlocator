769f1d861febb37f0d51ee138a05492c5b3e79a3
ParthaParizat <61804372+ParthaParizat@users.noreply.github.com>
2020-05-12 14:43:07 +0600

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,6 +1,6 @@
 var nation = 
 {
-    'update' : '12th May, 3:00PM' ,   
+    'update' : '12th May, 2:40PM' ,   
     'cases' : [16660 , 969],
     'death' : [250 , 11],
     'recovered' : [3147 , 254],  
