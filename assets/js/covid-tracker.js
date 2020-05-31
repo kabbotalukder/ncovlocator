eef10292e2f3c083599e0866baf7fe1ade9e3e85
Kabbo Talukder <63724484+kabbotalukder@users.noreply.github.com>
2020-05-31 17:30:23 +0600

    Update covid-tracker.js

--- assets/js/covid-tracker.js
+++ assets/js/covid-tracker.js
@@ -1,1 +1,101 @@
-var map;window.onload=function(){displayData(),displayDistData()};var infoWindow,locationSelect,markers=[],main_bound={north:27.382407993732738,east:95.31653598567469,south:19.463173036003,west:85.07727182551838};function initMap(){map=new google.maps.Map(document.getElementById("map"),{center:{lat:23.221057,lng:90.425433},zoom:11,disableDefaultUI:!0,restriction:{latLngBounds:main_bound,strictBounds:!1}}),infoWindow=new google.maps.InfoWindow,showStoresMarkers()}function displayData(){var e=nation.update,n=nation.cases[0],t=nation.cases[1],a=nation.death[0],o=nation.death[1],i=nation.recovered[0],s=nation.recovered[1];document.getElementById("update").innerHTML=e,document.getElementById("cases").innerHTML=n,document.getElementById("cases_daily").innerHTML=t,document.getElementById("death").innerHTML=a,document.getElementById("death_daily").innerHTML=o,document.getElementById("recovered").innerHTML=i,document.getElementById("recovered_daily").innerHTML=s}function showStoresMarkers(){var e=new google.maps.LatLngBounds;for(var[n,t]of districts.entries()){var a=new google.maps.LatLng(t.coordinates.latitude,t.coordinates.longitude),o=t.name,i=t.cases.total_cases,s=t.cases.total_deaths,d=t.cases.total_recoveries;e.extend(a),createMarker(a,o,i,s,d,n)}map.fitBounds(e)}function displayDistData(){var e="";for(var[n,t]of districts.entries()){var a=t.name,o=t.cases.total_cases;t.cases.total_deaths;e+=`\n      \n\n    <div class="dist-list-cont">\n    <div class="dist-list-viewer">\n        \n        <div class="dist-list">\n        ${a} \n        </div>\n\n        <div class="death-list">\n        ${o}\n        </div>\n\n        <div class="recover-list">\n        No Data\n        </div>\n\n    </div>\n    </div>\n    `,document.querySelector(".dist-list-cont").innerHTML=e}}function createMarker(e,n,t,a,o){var i="<b>"+n+"</b> </br>Total Case : "+t,s=new google.maps.Marker({map:map,position:e,animation:google.maps.Animation.DROP,label:{text:t.toString(),color:"black"}});google.maps.event.addListener(s,"click",function(){infoWindow.setContent(i),infoWindow.open(map,s,t)}),markers.push(s)}
+window.onload = function() {
+displayData();
+}
+var map;
+var markers = [];
+var infoWindow;
+var locationSelect;
+var main_bound = {
+north: 27.382407993732738,
+east: 95.31653598567469,
+south: 19.463173036003,
+west: 85.07727182551838,
+};
+function initMap() {
+// var location = {lat: 23.6850, lng: 90.3563};
+var location = {lat: 23.221057, lng: 90.425433}
+map = new google.maps.Map(document.getElementById('map'), {
+center: location,
+zoom: 11,
+disableDefaultUI: true,
+restriction: {
+latLngBounds: main_bound,
+strictBounds: false,
+},
+});
+infoWindow = new google.maps.InfoWindow();
+showStoresMarkers();
+}
+function displayData(){
+var update_time = nation['update'];
+var total_cases = nation['cases'][0];
+var daily_cases = nation['cases'][1];
+var total_death = nation['death'][0];
+var daily_death = nation['death'][1];
+var total_recovered = nation['recovered'][0];
+var daily_recovered = nation['recovered'][1];
+
+document.getElementById('update').innerHTML = update_time;
+document.getElementById('cases').innerHTML = total_cases;
+document.getElementById('cases_daily').innerHTML = daily_cases;
+document.getElementById('death').innerHTML = total_death;
+document.getElementById('death_daily').innerHTML = daily_death;
+document.getElementById('recovered').innerHTML = total_recovered;
+document.getElementById('recovered_daily').innerHTML = daily_recovered;
+
+}
+function showStoresMarkers(){
+var bounds = new google.maps.LatLngBounds();
+for(var [index , district] of districts.entries()){
+
+var latlng = new google.maps.LatLng(
+district['coordinates']['latitude'],
+district['coordinates']['longitude']
+);
+
+var name = district['name'];
+var total_cases = district['cases']['total_cases'];
+var total_deaths = district['cases']['total_deaths'];
+var total_recoveries = district['cases']['total_recoveries'];
+bounds.extend(latlng);
+createMarker(latlng, name,total_cases, total_deaths, total_recoveries, index);
+}
+map.fitBounds(bounds);
+}
+function createMarker(latlng, name, total_cases, total_deaths, total_recoveries){
+
+var clr,clr1,clr2,clr3;
+clr4 = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+clr3 = "http://maps.google.com/mapfiles/ms/icons/orange-dot.png"
+clr2 = "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
+clr1 = "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+
+if (total_cases == 0){
+  clr = clr3;
+}
+else if (total_cases < 50){
+  clr = clr2
+}
+else if(total_cases < 100){
+  clr = clr3;
+}
+else{
+  clr = clr4;
+}
+var html = "<b>" + name;
+
+  var marker = new google.maps.Marker({
+    map: map,
+    position: latlng,
+    animation: google.maps.Animation.DROP,
+    icon: {                             
+      url: clr
+    }
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker, total_cases);  
+  });
+  markers.push(marker);
+
+}
